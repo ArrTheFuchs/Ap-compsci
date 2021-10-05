0d0ae3e0d7bf09614ecfcad54dce7b29c2467a08
ArrtheFuchs <afuchs30@stuy.edu>
2021-10-04 20:30:36 -0400

    initial setup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,13 @@
+//Ariel Fuchs and the Duckler, Ruby Friedman and Tiffany, Joshua Gao and batman
+
+/* Disscoveries: It is possible to create a private call*/
+public class BigSib {
+    public static void main( String[] args ){
+        System.out.println( BigSib.Greet("Crosby") );
+        System.out.println( BigSib.Greet("Stills") );
+        System.out.println( BigSib.Greet("Nash") );
+    }
+private static String Greet(String s) {return "hello " + s;}
+
+
+}
