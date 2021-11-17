8fb914339346575f417f2503b95e32485fc6564c
ArrtheFuchs <afuchs30@stuy.edu>
2021-11-17 12:42:38 -0500

    array

---
+++ cb/test.java
@@ -0,0 +1,8 @@
+public class test{
+  public static void main(String[] args) {
+    Integer x = new Integer(3);
+    Integer y = 3;
+    System.out.println(x == y);
+    System.out.println (x.equals(y));
+  }
+}
