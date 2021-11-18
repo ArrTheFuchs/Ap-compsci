fff87fa3fcf7d3adbd6311ec89aae2fcddc5a714
ArrtheFuchs <afuchs30@stuy.edu>
2021-11-17 21:40:02 -0500

    Ne isveneti

--- 38_slots/Slots.java
+++ 38_slots/Slots.java
@@ -31,131 +31,149 @@ public class Slots {
   private String[] _fruits; //to be init'd by each instance
 
 
-  /*=====================================
-    Slots() -- default constructor
-    pre:  constant array FRUITS exists, has been initialized
-    post: mutable array _fruits contains same elements as FRUITS
-    =====================================*/
-  public Slots()
-  {
-    //allocate memory for _fruits based on size of FRUITS:
+   /*=====================================
+     Slots() -- default constructor
+     pre:  constant array FRUITS exists, has been initialized
+     post: mutable array _fruits contains same elements as FRUITS
+     =====================================*/
+   public Slots()
+   {
+
+     //allocate memory for _fruits based on size of FRUITS:
+    _fruits= new String[FRUITS.length];
+   for(int i=0; i<FRUITS.length; i +=1){
+
+   _fruits[i]=FRUITS[i];
+   }
+     //copy elements of FRUITS into _fruits:
+
+   }
 
 
-    //copy elements of FRUITS into _fruits:
-
-  }
-
-
-  /*=====================================
-    String toString() -- overrides inherited toString()
-    pre:
-    post: returns String of elements in slots 0 thru 2, separated by tabs
-    =====================================*/
-  public String toString()
-  {
-    return fruits[0] + "\t" + fruits[1] + "\t" + fruits[2];
-  }
-
-
-  /*=====================================
-    void swap(int,int) -- array swap util fxn
-    pre:  _fruits array exists
-    post: elements at indices i, j are swapped
-    =====================================*/
-/*  private void swap( int i, int j )
-  {
-
-  }
+   /*=====================================
+     String toString() -- overrides inherited toString()
+     pre:
+     post: returns String of elements in slots 0 thru 2, separated by tabs
+     =====================================*/
+   public String toString()
+   {
+     String output="";
+     for(int i=0; i<3; i+=1){
+       output+=_fruits[i]+ "  ";
+     }
+     return output;
+   }
 
 
-  /*=====================================
-    void spinOnce() -- simulate a pull of the slot machine lever
-    pre:  _fruits array exists
-    post: randomized order of elements in _fruits array
-    =====================================*/
-  /*public void spinOnce()
-  {
-    // A simple approach to shuffling:
-    // iterate through the array, swapping
-    // the val at each index with a randomly chosen other index
-    for(  )
-      swap(  );
-  }
+   /*=====================================
+     void swap(int,int) -- array swap util fxn
+     pre:  _fruits array exists
+     post: elements at indices i, j are swapped
+     =====================================*/
+   private void swap( int i, int j )
+   {
+   _fruits[i]=_fruits[j];
+   _fruits[j]=_fruits[i];
+   }
 
 
-  /*=====================================
-    boolean jackpot() -- checks for a winning combo
-    pre:  _fruits is existing array
-    post: returns true if first 3 slots represent winning combo,
-    false otherwise
-    =====================================*/
-/*  public boolean jackpot()
-  {
-    boolean retBoo = false;
+   /*=====================================
+     void spinOnce() -- simulate a pull of the slot machine lever
+     pre:  _fruits array exists
+     post: randomized order of elements in _fruits array
+     =====================================*/
+   public void spinOnce()
+   {
+     // A simple approach to shuffling:
+     // iterate through the array, swapping
+     // the val at each index with a randomly chosen other index
+     for(int i=0; i<FRUITS.length;i+=1  ){
+     int y= (int)(Math.random()*15);
+     swap(i, y);
+   }
+   String y = toString();
+ }
+
+   /*=====================================
+     boolean jackpot() -- checks for a winning combo
+     pre:  _fruits is existing array
+     post: returns true if first 3 slots represent winning combo,
+     false otherwise
+     =====================================*/
+   public boolean jackpot()
+   {
+     boolean retBoo = false;
+     if(_fruits[0].equals(_fruits[1])&&_fruits[2].equals(_fruits[1])){
+
+     retBoo = true;
+
+ }
+     return retBoo;
+   }
 
 
-    return retBoo;
-  }
+   /*=====================================
+     boolean miniWin() -- checks for a winning combo
+     pre:  _fruits is existing array
+     post: returns true if first 3 slots represent winning combo,
+     or if first 3 slots mutually distinct,
+     false otherwise
+     =====================================*/
+   public boolean miniWin()
+   {
+
+ if( jackpot()==true || !(_fruits[0].equals(_fruits[1])||_fruits[2].equals(_fruits[1])||(_fruits[0].equals(_fruits[2])))){
+
+     return true;
+   }
+ return false;
+ }
+
+   //main() method for testing
+   public static void main( String[] args ) {
+     //usage: move bar below down 1 line at a time to test functionality...
 
 
-  /*=====================================
-    boolean miniWin() -- checks for a winning combo
-    pre:  _fruits is existing array
-    post: returns true if first 3 slots represent winning combo,
-    or if first 3 slots mutually distinct,
-    false otherwise
-    =====================================*/
-/*  public boolean miniWin()
-  {
-    boolean retBoo = ?
+     Slots machine01 = new Slots();
+     Slots machine02 = new Slots();
 
+     //test to verify slot machines function indepently
+     System.out.println();
+     System.out.println( "Machine01 initial state:\t" + machine01 );
+     System.out.println( "Machine02 initial state:\t" + machine02 );
+     System.out.println( "\nSpinning machine01...\n" );
 
-    return retBoo;
-  } */
+     machine01.spinOnce();
+     System.out.println();
+     System.out.println( "Machine01 state:\t" + machine01 );
+     System.out.println( "Machine02 state:\t" + machine02 );
+     System.out.println();
 
+     //test gamble-until-you-win mechanism
+     System.out.println( "Preparing to spin until a mini win! . . ." );
+     System.out.println( "------------------------------------" );
 
-  //main() method for testing
-  public static void main( String[] args ) {
-    //usage: move bar below down 1 line at a time to test functionality...
+     //if you haven't won, spin again until you win!
+     while( machine01.miniWin() == false ) {
+       System.out.println( "Your spin..." + "\t" + machine01 );
+       System.out.println( "LOSE\n" );
+       machine01.spinOnce();
+     }
 
-    Slots machine01 = new Slots();
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    Slots machine02 = new Slots();/
-    //test to verify slot machines function indepently
-    System.out.println();
-    System.out.println( "Machine01 initial state:\t" + machine01 );
-    System.out.println( "Machine02 initial state:\t" + machine02 );
-    System.out.println( "\nSpinning machine01...\n" );
-    machine01.spinOnce();
-    System.out.println();
-    System.out.println( "Machine01 state:\t" + machine01 );
-    System.out.println( "Machine02 state:\t" + machine02 );
-    System.out.println();
-    //test gamble-until-you-win mechanism
-    System.out.println( "Preparing to spin until a mini win! . . ." );
-    System.out.println( "------------------------------------" );
-    //if you haven't won, spin again until you win!
-    while( machine01.miniWin() == false ) {
-      System.out.println( "Your spin..." + "\t" + machine01 );
-      System.out.println( "LOSE\n" );
-      machine01.spinOnce();
-    }
-    System.out.println( "====================================" );
-    //copy elements of FRUITS into _fruits:
-    System.out.println( "Your spin..." + "\t" + machine01 );
-    System.out.println( "WIN\n" );
-    System.out.println( "Preparing to spin until...jackpot! . . ." );
-    System.out.println( "------------------------------------" );
-    //if you haven't won, spin again until you win!
-    while( machine01.jackpot() == false ) {
-      System.out.println( "Your spin..." + "\t" + machine01 );
-      System.out.println( "LOSE\n" );
-      machine01.spinOnce();
-    }
-    System.out.println( "====================================" );
-    System.out.println( "Your spin..." + "\t" + machine01 );
-    System.out.println( "JACKPOT!\n" );
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  }//end main
+     System.out.println( "====================================" );
+     System.out.println( "Your spin..." + "\t" + machine01 );
+     System.out.println( "WIN\n" );
+     System.out.println( "Preparing to spin until...jackpot! . . ." );
+     System.out.println( "------------------------------------" );
+     //if you haven't won, spin again until you win!
+     while( machine01.jackpot() == false ) {
+       System.out.println( "Your spin..." + "\t" + machine01 );
+       System.out.println( "LOSE\n" );
+       machine01.spinOnce();
+     }
+     System.out.println( "====================================" );
+     System.out.println( "Your spin..." + "\t" + machine01 );
+     System.out.println( "JACKPOT!\n" );
+   }//end main
 
-}//end class Slots
+ }//end class Slots
