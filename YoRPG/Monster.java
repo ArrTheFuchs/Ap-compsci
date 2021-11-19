8efa0a4be882bce5b5b31225c36d79b22437804c
ArrtheFuchs <afuchs30@stuy.edu>
2021-11-19 13:20:16 -0500

    Slay thee monsters

---
+++ YoRPG/Monster.java
@@ -0,0 +1,6 @@
+public class Monster{
+
+  public static boolean isAlive(){
+
+  }
+}
---
+++ YoRPG/Protagonist.java
@@ -0,0 +1,7 @@
+public class Protagonist{
+  String name;
+
+  public Protagonist(String name){
+    this.name = name;
+  }
+}
