eca944df955ec7d0a564b5e58b0791eeb224f17a
ArrtheFuchs <afuchs30@stuy.edu>
2021-12-22 12:48:46 -0500

    good

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -51,7 +51,7 @@ public class Rational{
         /* return numerator */
         return numerator;
     }
-    
+
     public String toString(){
       /* returns string representation */
       String result = "";
@@ -60,18 +60,18 @@ public class Rational{
       result += "\n";
       return result;
     }
-        
+
     public double floatValue(){
         /* return floating point approx. using double */
         return numerator/(double)denominator; //will this do integer division?
     }
-    
+
     public void multiply(Rational multiple){
         /* multiplies object by the inputted object */
         numerator *= multiple.getNumerator();
         denominator *= multiple.getDenominator();
     }
-    
+
     public void divide(Rational divisor){
         /* divides this object by the other object */
         //KEEP CHANGE FLIP
@@ -91,14 +91,14 @@ public class Rational{
 
     public void add(Rational addend){
         this.numerator = (this.numerator * addend.denominator) + (addend.numerator * this.denominator);
-        this.denominator *= addend.denominator; 
+        this.denominator *= addend.denominator;
     }
-    
+
     public void subtract(Rational subtractant){
         this.numerator = (this.numerator * subtractant.denominator) - (subtractant.numerator * this.denominator);
-        this.denominator *= subtractant.denominator; 
+        this.denominator *= subtractant.denominator;
     }
-    
+
     //Static
     public static int gcd(int a, int b){
         int counter;
