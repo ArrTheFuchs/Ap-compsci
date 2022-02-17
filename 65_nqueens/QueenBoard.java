98d160d691811f43953be0cca14e40929cecaf04
ArrtheFuchs <afuchs30@stuy.edu>
2022-02-16 21:46:07 -0500

    cross

--- 65_nqueens/QueenBoard.java
+++ 65_nqueens/QueenBoard.java
@@ -1,21 +1,32 @@
 /***
+/***
+ * Stack Underflow: Ariel Fuchs, Russel Goyakevich, Nada hameed
+ *APCS
+HW 65 -- How Many Queens Can a Thinker Place, If a Thinker Can Place Queens -- Make a solution
+2022-02-16
+ *
+ *Disco
+ - visualizers are very helpful!
+ *
+ QCC:
+ Is there something recursion can do that loops can't?
+
+
  * class QueenBoard
- * Generates solutions for N-Queens problem.
+ * Generatees solutions for N-Queens problem.
  * USAGE:
- * 1. Peruse. Transcribe your KtS verbiage into block comments preceding each method where necessary.
+ * 1. Peruse. Transcribe your KtS verbiage into block comments preceding each
+ * method where necessary.
  * 2. Implement solver method.
  */
 
-public class QueenBoard
-{
+public class QueenBoard {
 
   private int[][] _board;
 
-  public QueenBoard( int size )
-  {
-    _board = new int[size][size];``
+  public QueenBoard(int size) {
+    _board = new int[size][size];
   }
-
 
   /***
    * precondition: board is filled with 0's only.
@@ -25,145 +36,135 @@ public class QueenBoard
    * If no solution, board is filled with 0's,
    * returns false.
    */
-  public boolean solve()
-  {
-    int rownum = 0
-    for(int i = 0; i < _board[].length)
+  public boolean solve() {
+    return solveH(0);
+  }
+
+  /**
+   * Helper method for solve.
+   */
+  private boolean solveH(int col) {
+    // returns true if algo completes meaning all queens on board
+    if (col == _board.length) {
+      return true;
+    }
+    // check for each row
+    for (int i = 0; i < _board.length; i++) {
+      // check if spot is availiable
+      if (_board[i][col] == 0) {
+        // adds the queen
+        addQueen(i, col);
+        // solves next column
+        if (solveH(col + 1)) {
+          return true;
+        }
+        // if solve doesnt work, remove the queen
+        removeQueen(i, col);
+      }
+    }
+    // there are no solutions
     return false;
   }
 
-
-  /**
-   *Helper method for solve.
-   */
-  private boolean solveH( int col )
-  {
-    for(int i = 0; i < _board[col].length; i++){
-      if(_board[row][col] == 0){
-        addQueen(col);
+  public void printSolution() {
+    /**
+     * Print board, a la toString...
+     * Except:
+     * all negs and 0's replaced with underscore
+     * all 1's replaced with 'Q'
+     */
+    String ans = "";
+    for (int r = 0; r < _board.length; r++) {
+      for (int c = 0; c < _board[0].length; c++) {
+        if ((_board[r][c] == 0) || (_board[r][c] < 0)) {
+          ans += "_" + "\t";
+        } else {
+          ans += "Q" + "\t";
+        }
       }
+      ans += "\n";
     }
+    System.out.println(ans);
   }
 
-
-  public void printSolution()
-  {
-    /** Print board, a la toString...
-        Except:
-        all negs and 0's replaced with underscore
-        all 1's replaced with 'Q'
-    */
-  }
-
-
-
-  //================= YE OLDE SEPARATOR =================
+  // ================= YE OLDE SEPARATOR =================
 
   /***
-   * <General description>
-   * precondition:
-   * postcondition:
+   * Adds queen to the board at _board[row][col] if position is valid
+   * precondition: that row and col are within range
+   * postcondition: returns false or queen is placed, and the areas it can attack
+   * are subtracted by 1 and returns true
    */
-  private boolean addQueen(int row, int col)
-  {
-    if(_board[row][col] != 0){
+  private boolean addQueen(int row, int col) {
+    if (_board[row][col] != 0) {
       return false;
     }
     _board[row][col] = 1;
     int offset = 1;
-    while(col+offset < _board[row].length){
-      _board[row][col+offset]--;
-      if(row - offset >= 0){
-        _board[row-offset][col+offset]--;    return false;
-
+    while (col + offset < _board[row].length) {
+      _board[row][col + offset]--;
+      if (row - offset >= 0) {
+        _board[row - offset][col + offset]--;
       }
-      if(row + offset < _board.length){
-        _board[row+offset][col+offset]--;
+      if (row + offset < _board.length) {
+        _board[row + offset][col + offset]--;
       }
       offset++;
     }
     return true;
   }
 
-
   /***
-   * <General description>
-   * precondition:
-   * postcondition:
+   * Removes queen at _board[row][col] if there is one ther
+   * precondition: that row and col are within range
+   * postcondition: returns false or queen is removed, and the areas it previously
+   * could attack are added to by 1 and returns true
    */
-  private boolean removeQueen(int row, int col){
-    if ( _board[row][col] != 1 ) {
+  private boolean removeQueen(int row, int col) {
+    if (_board[row][col] != 1) {
       return false;
     }
     _board[row][col] = 0;
     int offset = 1;
 
-    while( col+offset < _board[row].length ) {
-      _board[row][col+offset]++;
-      if( row - offset >= 0 ) {
-        _board[row-offset][col+offset]++;
+    while (col + offset < _board[row].length) {
+      _board[row][col + offset]++;
+      if (row - offset >= 0) {
+        _board[row - offset][col + offset]++;
       }
-      if( row + offset < _board.length ) {
-        _board[row+offset][col+offset]++;
+      if (row + offset < _board.length) {
+        _board[row + offset][col + offset]++;
       }
       offset++;
     }
     return true;
   }
 
-
   /***
-   * <General description>
+   * <general description>
    * precondition:
    * postcondition:
    */
-  public String  toString()
-  {
+  public String toString() {
     String ans = "";
-    for( int r = 0; r < _board.length; r++ ) {
-      for( int c = 0; c < _board[0].length; c++ ) {
-        ans += _board[r][c]+"\t";
+    for (int r = 0; r < _board.length; r++) {
+      for (int c = 0; c < _board[0].length; c++) {
+        ans += _board[r][c] + "\t";
       }
       ans += "\n";
     }
     return ans;
   }
 
-
-  //main method for testing...
-  public static void main( String[] args )
-  {
-    QueenBoard b = new QueenBoard(5);
+  // main method for testing...
+  public static void main(String[] args) {
+    QueenBoard b = new QueenBoard(8);
     System.out.println(b);
-    /** should be...
-       0	0	0	0	0
-       0	0	0	0	0
-       0	0	0	0	0
-       0	0	0	0	0
-       0	0	0	0	0
-    */
 
-    b.addQueen(3,0);
-    b.addQueen(0,1);
+    b.solve();
     System.out.println(b);
-    /** should be...
-       0	1	-1	-2	-1
-       0	0	-2	0	0
-       0	-1	0	-1	0
-       1	-1	-1	-1	-2
-       0	-1	0	0	0
-    */
-
-    b.removeQueen(3,0);
-    System.out.println(b);
-    /** should be...
-       0	1	-1	-1	-1
-       0	0	-1	0	0
-       0	0	0	-1	0
-       0	0	0	0	-1
-       0	0	0	0	0
-    */
+    b.printSolution();
 
   }
 
-}//end class
+}// end class
