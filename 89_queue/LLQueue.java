d03f45fa3b0d3cb2abce6390e1d25a38a79c6904
ArrtheFuchs <afuchs30@stuy.edu>
2022-04-12 11:03:24 -0400

    sorry

--- 89_queue/LLQueue.java
+++ 89_queue/LLQueue.java
@@ -8,34 +8,26 @@ time spent: 1.2hrs
 DISCO:
   - This felt very similar to LLStack and ALStack
   - We don't need to check isFull because we can always add things onto the end of our queues.
-
+  
 QCC:
   - In ALQueue, how can we both enqueue and dequeue in constant time?
 */
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class ALQueue<QUASAR> implements Queue<QUASAR>{
+public class LLQueue<QUASAR> implements Queue<QUASAR>{
   //inst vars
-  private ArrayList<QUASAR> _queue;
+  LinkedList<QUASAR> _queue;
 
-  public ALQueue() {
-    _queue = new ArrayList<QUASAR>();
+  public LLQueue() {
+    _queue = new LinkedList<QUASAR>();
   } // O(1)
-
-  public QUASAR peekFront() {
-    return _queue.get(0);
-  }
-
-  public boolean isEmpty() {
-    return _queue.size() == 0;
-  }
 
   public void enqueue( QUASAR s )
   {
-    _queue.add(s);
+    _queue.addLast(s);
     return;
-  }// O(1) because the algo does not iterate through the queue. it just adds a
+  }// O(1) because the algo does not iterate through the stack. it just adds a
   // new element to the end
 
 
@@ -46,10 +38,22 @@ public class ALQueue<QUASAR> implements Queue<QUASAR>{
     QUASAR blah = _queue.get(0);
     _queue.remove(0);
     return blah;
-  } // O(n)
+  }// O(1) because the algo does not iterate through the stack. it just removes a
+  // new element to the end
 
-  public static void main(String[] args){
-    ALQueue<Integer> hotCakes = new ALQueue<Integer>();
+  public QUASAR peekFront() {
+    if (_queue.isEmpty()) return null;
+    return (_queue.getFirst());
+  }// O(1)
+
+  //chk for emptiness
+  public boolean isEmpty()
+  {
+    return _queue.size() == 0;
+  }// O(1) because it is not dependent on the size of the stack.
+
+  public static void main(String[] args) {
+    LLQueue<Integer> hotCakes = new LLQueue<Integer>();
     hotCakes.enqueue(1);
     hotCakes.enqueue(2);
     hotCakes.enqueue(3);
@@ -63,6 +67,7 @@ public class ALQueue<QUASAR> implements Queue<QUASAR>{
     System.out.println(hotCakes.dequeue());
     System.out.println(hotCakes.dequeue());
     System.out.println(hotCakes.dequeue());
+
 
     System.out.println("finished dequeuing no more cake");
   }
